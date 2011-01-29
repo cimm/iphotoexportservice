6a67714bccb213f2db7e403da50bfa5506eb4725
Simon Schoeters <hamfilter@gmail.com>
2011-01-29 21:52:45 +0100

    Add a version number

--- Info.lua
+++ Info.lua
@@ -3,6 +3,7 @@ return {
   LrSdkMinimumVersion = 1.3,
 
   LrToolkitIdentifier = 'be.suffix.lightroom.iphotoexportservice',
+  VERSION = { major = 1, minor = 0, revision = 0, build = 0, },
 
   LrExportServiceProvider = {
     title = "iPhoto",  -- this string appears as the export destination
